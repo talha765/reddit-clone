8162772aec42f495fb879eb1e6b8d45d931411a3
Talha <wajidtalha9@gmail.com>
2024-09-09 23:37:47 +0500

    search added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import logo from "./assets/reddit_logo.png"
-function App() {
+import './App.css';
+import logo from './assets/reddit_logo.png';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
+function App() {
   return (
-   <div >
-    <header className="flex bg-reddit_dark">
-     <img src={logo} className='w-8 h-8 ml-3 mt-3 mb-3 bg-reddit_dark '/>
-    </header>
-   </div>
-  )
+    <div>
+      <header className="flex bg-gray-900 p-3"> {/* Adjust padding */}
+        <div className="mx-4">
+          <img src={logo} className="w-8 h-8 mt-1 mb-1" alt="Reddit logo" />
+        </div>
+        <form action="" className="relative flex items-center bg-gray-800 rounded-lg">
+          <input 
+            type="text" 
+            className="bg-gray-800 h-8 text-white pl-8 pr-2 rounded-lg" // Added padding-left for icon space
+            placeholder="Search" 
+          />
+          <MagnifyingGlassIcon className="absolute left-2 w-5 h-5 text-gray-400" /> {/* Positioned inside input */}
+        </form>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
