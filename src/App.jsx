c8891be20aecfae3139fa0470247877e315b4005
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 17:37:55 +0500

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './Index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import LandingPage from "./Components/LandingPage"
-import HomePage from "./Components/HomePage"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from "./Components/LandingPage";
+import HomePage from "./Components/HomePage";
+import LoginPage from "./Components/LoginPage"; // Import the login page
+
 const App = () => (
   <Router>
     <div className="flex flex-col h-screen">
-       {/* <HomePage /> */}
-      <LandingPage />
+      <Routes>
+        {/* Add route for HomePage */}
+        <Route path="/home" element={<HomePage />} />
+
+        {/* Add route for LoginPage */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Default route: LandingPage */}
+        <Route path="/land" element={<LandingPage />} />
+      </Routes>
     </div>
   </Router>
 );
